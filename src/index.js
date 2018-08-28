533d1cb21db98b2279ac71d6ac28bdeb31c5fe17
Alexander Hadik <alex@transcriptic.com>
2018-08-28 15:22:12 -0700

    Upgrade babel

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ module.exports = function jcsPlugin(babel) {
   };
 
   return {
-    inherits: require("babel-plugin-syntax-jsx"),
+    inherits: require("@babel/plugin-syntax-jsx"),
     visitor: visitor
   };
 };
